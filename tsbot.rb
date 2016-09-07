9fc898672b1d6d281853352f2e9e7878082ca883
Aleks Dapkus <acdapkus@gmail.com>
2016-09-07 15:42:20 -0700

    Fixed global vars; Added auto-deleted of .wav

--- tsbot.rb
+++ tsbot.rb
@@ -15,7 +15,7 @@ require 'osc-ruby'
   def stop_machine_custom (m)
     name = m.message.split(' ')[1]
     name = sanitize_filename(name)
-    name = save_location + name
+    name = $save_location + "/" + name
     if File.exists?(name)
       m.reply("Already Exists, pls again")
     else
@@ -23,65 +23,60 @@ require 'osc-ruby'
     end
     stop = "oscsend localhost 7133 /stop"
     system(stop)
-    spam = Dir.glob(save_location + "/*").max_by {|f| File.mtime(f)}
+    spam = Dir.glob($save_location + "/*").max_by {|f| File.mtime(f)}
     File.rename(spam,name)
     convert2mp3(name)
 #delete here
     File.delete(name)
-    spam = Dir.glob(save_location + "/*").max_by {|f| File.mtime(f)}
+    spam = Dir.glob($save_location + "/*").max_by {|f| File.mtime(f)}
     m.reply("Converting to MP3")
     spam = spam.split('/').last
-    m.reply(url + spam)
+    m.reply($url + spam)
   end
 
   def stop_machine (m)
     m.reply("Stop Recording")
     stop = "oscsend localhost 7133 /stop"
     system(stop)
-    spam = Dir.glob(save_location + "/*").max_by {|f| File.mtime(f)}
+    spam = Dir.glob($save_location + "/*").max_by {|f| File.mtime(f)}
     convert2mp3(spam)
 #delete here
     File.delete(spam)
-    spam = Dir.glob(save_location + "/*").max_by {|f| File.mtime(f)}
+    spam = Dir.glob($save_location + "/*").max_by {|f| File.mtime(f)}
     m.reply("Converting to MP3")
     spam = spam.split('/').last
-    m.reply(url + spam)
+    m.reply($url + spam)
   end
 
   def convert2mp3 (f)
     `ffmpeg -i #{f} -f mp3 #{f}.mp3`
   end
 
-save_location = ''
-url = ''
-irc_server = ''
-irc_channel = ''
-nickname = ''
+$save_location = ''
+$url = ''
+$irc_server = ''
+$irc_channel = ''
+$nickname = ''
 
 IO.foreach("config") do |line|
   line.chomp!
   key, value = line.split(nil, 2)
   case key
   when /^([#;]|$)/; #Ignore line
-  when "SAVELOCATION"; save_location = value
-  when "URL"; url = value
-  when "IRCSERVER"; irc_server = value
-  when "IRCCHANNEL"; irc_channel = "#" + value
-  when "NICKNAME"; nickname = value
+  when "SAVELOCATION"; $save_location = value
+  when "URL"; $url = value
+  when "IRCSERVER"; $irc_server = value
+  when "IRCCHANNEL"; $irc_channel = "#" + value
+  when "NICKNAME"; $nickname = value
   when /^./; puts "#{key}: unknown key"
   end
 end
 
-puts "#{save_location}"
-puts url
-puts irc_server
-puts irc_channel
-
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = irc_server
-    c.channels = [irc_channel]
-    c.nick = nickname
+    c.server = $irc_server
+    c.channels = [$irc_channel]
+    c.nick = $nickname
   end
 
   on :message do |m|
